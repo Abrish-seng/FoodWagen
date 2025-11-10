a9a69d3f35d057e50329b1c5b6f0a25ce3aa6883
Abrish-seng <abrish814@gmail.com>
2025-11-10 16:45:24 +0300

    adding the layout page

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'FoodWagen - Food Delivery & Pickup',
+  description: 'Within a few clicks, find meals that are accessible near you',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
